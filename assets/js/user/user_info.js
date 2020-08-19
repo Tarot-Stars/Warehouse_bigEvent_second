a4282887adba152934b49668a776547c22ecc5da
zhaojia <1363043290@qq.com>
2020-08-19 22:57:58 +0800

    暂时提交

---
+++ assets/js/user/user_info.js
@@ -0,0 +1,45 @@
+$(function () {
+    var form = layui.form;
+    var layer = layui.layer;
+    form.verify({
+        nickname: function (value) {
+            if (value.length > 6) return '昵称必须在 1 ~ 6 个字符之间';
+        }
+    });
+    initUserInfo();
+    // 初始化用户的基本信息
+    function initUserInfo() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            success: function (res) {
+                if (res.status !== 0) return layer.msg(res.message);
+                // console.log(res);
+                form.val('formUserInfo', res.data);
+            }
+        })
+    }
+    // 重置表单数据
+    $('#btnReset').on('click', function (e) {
+        // 阻止按钮的默认行为
+        e.preventDefault();
+        initUserInfo();
+    });
+    // 监听表单的提交事件
+    $('.layui-form').on('click', function (e) {
+        // 阻止表单的默认提交行为
+        e.preventDefault();
+        // 发起 Ajax 请求
+        $.ajax({
+            method: 'POST',
+            url: '/my/userinfo',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) return layer.msg(res.message);
+                layer.msg('用户信息修改成功');
+                // 调用父页面中的方法，重新渲染用户的头像和用户的信息
+                window.parent.getUserInfo();
+            }
+        });
+    });
+});
